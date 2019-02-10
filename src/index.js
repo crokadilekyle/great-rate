1d60ae03c641ece96c304c40a6cc3231f90bc994
Mathew Gries <mgries@temenos.com>
2019-02-10 16:12:41 -0500

    Fixed applyMiddleware export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
+import middleware from './middleware'
 import './index.css'
 import App from './components/App'
 
+const store = createStore(reducer, middleware)
+console.log(store)
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     document.getElementById('root')
 )
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,4 @@ import logger from './logger'
 import thunk from 'redux-thunk'
 import { applyMiddleware } from 'redux'
 
-export default applyMiddleware({
-    thunk,
-    logger,
-})
+export default applyMiddleware(thunk,logger)
