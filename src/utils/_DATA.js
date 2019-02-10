b31209c5efa4c4148db44c978bf2288214c93daa
Mathew Gries <mgries@temenos.com>
2019-02-10 15:17:32 -0500

    Mock data

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,5 +2,76 @@ let users = {
     mathewgries: {
         id: 'mathewgries',
         name: 'Mathew Gries',
+        avatarURL: 'https://avatars1.githubusercontent.com/u/12651184?v=4',
+        categories: ['ixwyfdzuc9gg40756fua'],
+        submissions: {
+            "ixwyfdzuc9gg40756fua": 'rzd4et1iaonce3hxpvpfu9',
+        },
+        votes: {
+
+        },
+    },
+    chisgossger: {
+        id: 'chrisgossger',
+        name: 'Christopher Gossger',
+        avatarURL: 'https://image.shutterstock.com/image-photo/headshot-satisfied-cheerful-handsome-man-260nw-793117360.jpg',
+        categories : ['eg381p6p9wwyil0o1jvd7a'],
+        submissions: {
+            "ixwyfdzuc9gg40756fua": 'yxl1w2bky5bei560w0zl88',
+            "eg381p6p9wwyil0o1jvd7a": 'yxb4d8irh9fcr43nxgrh4f',
+        }
     }
 }
+
+let categories = {
+    ixwyfdzuc9gg40756fua: {
+        id: 'ixwyfdzuc9gg40756fua',
+        name: 'Best Poop',
+        description: 'Vote for the best poop photo',
+    },
+    eg381p6p9wwyil0o1jvd7a: {
+        id: 'eg381p6p9wwyil0o1jvd7a',
+        name: 'Cool Cars',
+        description: 'Vote for the coolest car',
+    },
+}
+
+submissions = {
+    rzd4et1iaonce3hxpvpfu9: {
+        id: 'rzd4et1iaonce3hxpvpfu9',
+        category: 'ixwyfdzuc9gg40756fua',
+        description: 'My poop',
+        imageURL: 'https://i.etsystatic.com/5176714/d/il/2924fd/393927691/il_340x270.393927691_565h.jpg?version=0',
+        votes: {
+            total: 10,
+            upvotes: 7,
+            downvotes: 3,
+        }
+    },
+    yxl1w2bky5bei560w0zl88: {
+        id: 'yxl1w2bky5bei560w0zl88',
+        category: 'ixwyfdzuc9gg40756fua',
+        description: 'Robo poop',
+        imageURL: 'https://cdn.thisiswhyimbroke.com/images/dorodorobo-poop-robot-640x534.jpg',
+        votes: {
+            total: 15,
+            upvotes: 14,
+            downvotes: 1,
+        }
+    },
+    yxb4d8irh9fcr43nxgrh4f: {
+        id: 'yxb4d8irh9fcr43nxgrh4f',
+        category: 'eg381p6p9wwyil0o1jvd7a',
+        description: 'Urkel mobile',
+        imageURL: 'https://media.wired.com/photos/59326f55edfced5820d10640/1:1/w_2400,c_limit/tiny-classic-cars-ft.jpg',
+        votes: {
+            total: 7,
+            upvotes: 2,
+            downvotes: 5,
+        }
+    },
+}
+
+function generateUID () {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+  }
