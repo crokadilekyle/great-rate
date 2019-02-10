3583723c407d84474aa7223a3be769e27cb594f6
Mathew Gries <mgries@temenos.com>
2019-02-10 16:34:49 -0500

    HandleInitialData

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+
 
 class App extends Component {
+	
+	componentDidMount = () => {
+		this.props.dispatch(handleInitialData())
+	}
+	
 	render() {
+
 		return (
 			<div>
-				START APP
+				STARTER CODE
 			</div>
 		)
 	}
 }
 
-export default App
+export default connect()(App)
